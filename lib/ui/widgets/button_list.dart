888fa3bf10a681e0f12beba5fd6e04409e5ccb6d
Andrés Prieto <pipeprieto0902@gmail.com>
2023-03-14 21:57:16 -0500

    Add: Icons Listview

--- lib/ui/widgets/button_list.dart
+++ lib/ui/widgets/button_list.dart
@@ -6,16 +6,63 @@ class ButtonList extends StatelessWidget {
     final buttonIconList = Row(
       children: [
         Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30.0), color: Colors.white),
-          child: Icon(
-            Icons.bookmark_outline,
-            color: Colors.blue,
-            size: 40.0,
-          ),
-          height: 50.0,
-          width: 50.0,
-        )
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0), color: Colors.white),
+            child: Icon(
+              Icons.bookmark_outline,
+              color: Colors.blue,
+              size: 20.0,
+            ),
+            height: 30.0,
+            width: 30.0,
+            margin: EdgeInsets.only(left: 40.0)),
+        Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.grey.shade400),
+            child: Icon(
+              Icons.bookmark_outline,
+              color: Colors.blue,
+              size: 20.0,
+            ),
+            height: 30.0,
+            width: 30.0,
+            margin: EdgeInsets.only(left: 40.0)),
+        Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0), color: Colors.white),
+            child: Icon(
+              Icons.add,
+              color: Colors.blue,
+              size: 40.0,
+            ),
+            height: 50.0,
+            width: 50.0,
+            margin: EdgeInsets.only(left: 40.0)),
+        Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.grey.shade400),
+            child: Icon(
+              Icons.email_outlined,
+              color: Colors.blue,
+              size: 20.0,
+            ),
+            height: 30.0,
+            width: 30.0,
+            margin: EdgeInsets.only(left: 40.0)),
+        Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.grey.shade400),
+            child: Icon(
+              Icons.person_outline_sharp,
+              color: Colors.blue,
+              size: 20.0,
+            ),
+            height: 30.0,
+            width: 30.0,
+            margin: EdgeInsets.only(left: 40.0))
       ],
     );
 
