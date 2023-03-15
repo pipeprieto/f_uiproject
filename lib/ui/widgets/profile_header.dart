bddcf8894bb5cd6990734ceeed5c285818a85ed9
Andrés Prieto <pipeprieto0902@gmail.com>
2023-03-14 21:57:35 -0500

    Add: Profile header

--- lib/ui/widgets/profile_header.dart
+++ lib/ui/widgets/profile_header.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myapp/ui/widgets/button_list.dart';
 import 'package:myapp/ui/widgets/profile_info.dart';
 import 'gradient_background.dart';
+import 'landscape_card_list.dart';
 
 class ProfileHeader extends StatelessWidget {
   @override
@@ -14,10 +15,12 @@ class ProfileHeader extends StatelessWidget {
           children: [
             ProfileInfo(
                 "assets/img/people.jpg", "Pathum Zoo", "pathumzoo1@gmail.com"),
-            ButtonList()
+            ButtonList(),
           ],
-        )
+        ),
+        LandscapeCardList(),
       ],
     );
   }
 }
+// LandscapeCardList(),
